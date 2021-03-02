7c74d26d78e0793eecb44be5bd394e38ef04128d
Marcis Jurkons <marcis.jurkons@hotmail.com>
2021-03-02 13:50:42 +0200

    Simple http server for sensor measures

---
+++ MetricHttpServer/Data/MetricsData.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace MetricHttpServer.Data
+{
+    static class MetricsData
+    {
+        /// <summary>
+        /// Get list of sensor measures in specific date for each metric
+        /// </summary>
+        /// <param name="date">Date for which to get metrics data</param>
+        /// <returns>List of metrics data</returns>
+        public static List<object> GetSensorMeasuresForDate(string date)
+        {
+            var result = new List<object>();
+
+            using (var connection = new SqliteConnection("Data Source = Data/aranet.db"))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"SELECT 
+                        s.sensor_id,
+                        s.name as sensor_name,
+                        m.metric_name,
+                        u.unit_name,
+                        sm.min_measure_value,
+                        sm.max_measure_value
+                        FROM 
+                        sensors s
+                        INNER JOIN (
+                        SELECT
+                        m.sensor_id,
+                        m.metric_id,
+                        MAX(m.rvalue) as max_measure_value,
+                        MIN(m.rvalue) as min_measure_value
+                        FROM
+                        measures m
+                        WHERE date(m.rtime) = @measure_date
+                        GROUP BY sensor_id, metric_id) sm on s.sensor_id = sm.sensor_id
+                        INNER JOIN metrics m on sm.metric_id = m.metric_id
+                        INNER JOIN units u on m.unit_id = u.unit_id";
+
+                    command.Parameters.Add(new SqliteParameter
+                    {
+                        ParameterName = "@measure_date",
+                        Value = date
+                    });
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(new
+                            {
+                                SensorId = (long)reader["sensor_id"],
+                                SensorName = reader["sensor_name"].ToString(),
+                                MetricName = reader["metric_name"].ToString(),
+                                UnitName = reader["unit_name"].ToString(),
+                                MinValue = (double)reader["min_measure_value"],
+                                MaxValue = (double)reader["max_measure_value"]
+                            });
+                        }
+                    }
+
+                    return result;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get list of all sensors with last measure value and date
+        /// </summary>
+        /// <returns>List of measure data</returns>
+        public static List<object> GetSensorListWithLastMeasure()
+        {
+            var result = new List<object>();
+            using (var connection = new SqliteConnection("Data Source = Data/aranet.db"))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"SELECT
+                        s.sensor_id,
+                        s.serial_code,
+                        s.name as sensor_name,
+                        datetime(mo.rtime) as measure_date,
+                        mo.rvalue as measure_value,
+                        me.metric_name,
+                        u.unit_name
+                        FROM sensors s
+                        LEFT JOIN (SELECT
+                        m.sensor_id,
+                        MAX(m.rtime) as last_measure_date,
+                        MAX(m.reading_id) as max_reading_id
+                        FROM measures m
+                        GROUP BY m.sensor_id) md ON s.sensor_id = md.sensor_id 
+                        LEFT JOIN measures mo on md.last_measure_date = mo.rtime
+                        LEFT JOIN metrics me on mo.metric_id = me.metric_id
+                        LEFT JOIN units u on me.unit_id = u.unit_id";
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new
+                            {
+                                SensorId = (long)reader["sensor_id"],
+                                SerialCode = reader["serial_code"].ToString(),
+                                Name = reader["sensor_name"].ToString(),
+                                LastMeasureDate = reader["measure_date"] == DBNull.Value ? null : reader["measure_date"].ToString(),
+                                LastMeasureValue = reader["measure_value"] as double?,
+                                MetricName = reader["metric_name"] == DBNull.Value ? null : reader["metric_name"].ToString(),
+                                UnitName = reader["unit_name"] == DBNull.Value ? null : reader["unit_name"].ToString()
+                            };
+
+                            result.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MetricHttpServer/Program.cs
@@ -0,0 +1,114 @@
+﻿using MetricHttpServer.Data;
+using System;
+using System.Net;
+using System.Text;
+
+namespace MetricHttpServer
+{
+    class Program
+    {
+        private static HttpListener httpListener;
+
+        /// <summary>
+        /// Main entry point for the application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var port = System.Configuration.ConfigurationManager.AppSettings["metricsHttpServerPort"];
+
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add($"http://127.0.0.1:{port}/");
+            httpListener.Start();
+
+            ProcessFirstOrNextRequest();
+            Console.WriteLine("Press anny key to exit application...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Process http requests
+        /// </summary>
+        private static void ProcessFirstOrNextRequest()
+        {
+            // Start to get connection context
+            var asyncResult = httpListener.BeginGetContext(OnGetContext, httpListener);
+        }
+
+        /// <summary>
+        /// Called when request has been made
+        /// </summary>
+        /// <param name="asyncResult"></param>
+        private static void OnGetContext(IAsyncResult asyncResult)
+        {
+            var context = httpListener.EndGetContext(asyncResult);
+            var urlExists = false;
+
+            // There are only two types of urls avaiable
+            // Made simple check to get data for each of them
+            if (context.Request.Url.Segments.Length == 2)
+            {
+                // If url is http://127.0.0.1:{port}/sensors
+                if (context.Request.Url.Segments[1] == "sensors")
+                {
+                    context.Response.StatusCode = 200;
+                    context.Response.StatusDescription = "OK";
+                    context.Response.ContentType = "application/json";
+
+                    try
+                    {
+                        var sensorList = MetricsData.GetSensorListWithLastMeasure();
+                        var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(sensorList);
+                        var byteData = Encoding.UTF8.GetBytes(jsonData);
+                        context.Response.OutputStream.Write(byteData, 0, byteData.Length);
+                    }
+                    catch
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.StatusDescription = "Internal Server Error";
+                    }
+                    context.Response.Close();
+                    urlExists = true;
+                }
+            }
+            else if (context.Request.Url.Segments.Length == 3)
+            {
+                // If url is http://127.0.0.1:{port}/measures/2019-09-09
+                if (context.Request.Url.Segments[1].Replace("/", string.Empty) == "measures")
+                {
+                    var date = context.Request.Url.Segments[2];
+                    context.Response.StatusCode = 200;
+                    context.Response.StatusDescription = "OK";
+                    context.Response.ContentType = "application/json";
+
+                    try
+                    {
+                        var measureList = MetricsData.GetSensorMeasuresForDate(date);
+                        var jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(measureList);
+                        var byteData = Encoding.UTF8.GetBytes(jsonData);
+                        context.Response.OutputStream.Write(byteData, 0, byteData.Length);
+                    }
+                    catch
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.StatusDescription = "Internal Server Error";
+                    }
+
+                    context.Response.Close();
+                    urlExists = true;
+                }
+            }
+
+            // If wrong url specified return 404 Not Found error
+            if (!urlExists)
+            {
+                context.Response.StatusCode = 404;
+                context.Response.StatusDescription = "Not Found";
+                context.Response.Close();
+            }
+
+            // Process next request
+            ProcessFirstOrNextRequest();
+        }
+    }
+}
